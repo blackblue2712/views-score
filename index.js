3826a8be109a986875ff780a325970e9151e4591
Nghia <nghia.dang@urbn8.com>
2020-06-09 23:26:29 +0700

    fix date

--- index.js
+++ index.js
@@ -1,54 +1,66 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const axios = require("axios");
-const compression = require("compression")
+const compression = require("compression");
 
-// const { change_alias } = require("./util/helper");
+// const { xlSerialToJsDate } = require("./util/helper");
 
 const port = process.env.PORT || 3000;
 const app = express();
-app.set('view engine', 'pug');
-app.use(compression())
+app.set("view engine", "pug");
+app.use(compression());
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-app.use(express.static('public'));
-
+app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
-	try {
-		const response = await axios.get("https://views-score.firebaseio.com/views-score/hp.json");
-		console.log(Object.keys(response.data))
-		res.render("index", { hps: Object.keys(response.data) })
-	} catch (err) {
-		res.render("index", { hps: [] })
-	}
-})
+  try {
+    const response = await axios.get(
+      "https://views-score.firebaseio.com/views-score/hp.json"
+    );
+    console.log(Object.keys(response.data));
+    res.render("index", { hps: Object.keys(response.data) });
+  } catch (err) {
+    res.render("index", { hps: [] });
+  }
+});
 
 app.post("/score/hp", async (req, res) => {
-	const { hp, mssv } = req.body;
-	const response = await axios.get("https://views-score.firebaseio.com/views-score/score/" + hp + ".json");
-	console.log(response.data)
+  const { hp, mssv, dayOfBirth } = req.body;
+  const timeCheck = new Date(dayOfBirth).getTime();
+  const response = await axios.get(
+    "https://views-score.firebaseio.com/views-score/score/" + hp + ".json"
+  );
 
-	let result = response.data.find(res => {
-		return res.id === mssv;
-	})
-	res.send(result)
-})
+  let result = response.data.find((res) => {
+    return res.id === mssv && new Date(res.dayOfBirth).getTime() === timeCheck;
+  });
+  res.send(result);
+});
 
 app.post("/score/add-hp", async (req, res) => {
-	const { hpStringify, dataFilter } = req.body;
+  const { hpStringify, dataFilter } = req.body;
 
-	try {
-		await Promise.all([
-			axios.put("https://views-score.firebaseio.com/views-score/score/" + hpStringify + ".json", dataFilter),
-			axios.put("https://views-score.firebaseio.com/views-score/hp/" + hpStringify + ".json", { add: true })
-		]);
-		res.send({ message: "done" })
-	} catch (err) {
-		res.send({ message: "fail" })
-	}
-})
-
+  try {
+    await Promise.all([
+      axios.put(
+        "https://views-score.firebaseio.com/views-score/score/" +
+          hpStringify +
+          ".json",
+        dataFilter
+      ),
+      axios.put(
+        "https://views-score.firebaseio.com/views-score/hp/" +
+          hpStringify +
+          ".json",
+        { add: true }
+      ),
+    ]);
+    res.send({ message: "done" });
+  } catch (err) {
+    res.send({ message: "fail" });
+  }
+});
 
 // Using template engines
 // app.get('/', async function (req, res) {
@@ -59,5 +71,5 @@ app.post("/score/add-hp", async (req, res) => {
 // });
 
 app.listen(port, function () {
-	console.log('Example app listen on port ' + port);
+  console.log("Example app listen on port " + port);
 });
--- public/js/index.js
+++ public/js/index.js
@@ -1,103 +1,128 @@
 let file;
 
 function change_alias(alias) {
-	var str = alias.replace(/\s/g, "");
-	str = str.toLowerCase();
-	str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, "a");
-	str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, "e");
-	str = str.replace(/ì|í|ị|ỉ|ĩ/g, "i");
-	str = str.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g, "o");
-	str = str.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g, "u");
-	str = str.replace(/ỳ|ý|ỵ|ỷ|ỹ/g, "y");
-	str = str.replace(/đ/g, "d");
-	// str = str.replace(/!|@|%|\^|\*|\(|\)|\+|\=|\<|\>|\?|\/|,|\.|\:|\;|\'|\"|\&|\#|\[|\]|~|\$|_|`|-|{|}|\||\\/g, " ");
-	str = str.replace(/ + /g, " ");
-	str = str.trim();
-	return str;
+  var str = alias.replace(/\s/g, "");
+  str = str.toLowerCase();
+  str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, "a");
+  str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, "e");
+  str = str.replace(/ì|í|ị|ỉ|ĩ/g, "i");
+  str = str.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g, "o");
+  str = str.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g, "u");
+  str = str.replace(/ỳ|ý|ỵ|ỷ|ỹ/g, "y");
+  str = str.replace(/đ/g, "d");
+  // str = str.replace(/!|@|%|\^|\*|\(|\)|\+|\=|\<|\>|\?|\/|,|\.|\:|\;|\'|\"|\&|\#|\[|\]|~|\$|_|`|-|{|}|\||\\/g, " ");
+  str = str.replace(/ + /g, " ");
+  str = str.trim();
+  return str;
 }
 
-const onSubmitFormAddData = async e => {
-	e.preventDefault();
-
-	try {
-		const data = await readFile(file);
-		const hpStringify = change_alias(data.hp);
-
-		await axios.post("/score/add-hp", { hpStringify, dataFilter: data.dataFilter });
-	} catch (err) {
-		console.log(err)
-	}
+function changeOrderDate(date) {
+  let splitDate = date.split("/");
+  return splitDate[1] + "/" + splitDate[0] + "/" + splitDate[2];
 }
 
-const onChangeFile = async e => {
-	file = e.target.files[0];
+function xlSerialToJsDate(xlSerial) {
+  return new Date(
+    -2209075200000 + (xlSerial - (xlSerial < 61 ? 0 : 1)) * 86400000
+  ).toLocaleDateString();
 }
+
+const onSubmitFormAddData = async (e) => {
+  e.preventDefault();
+
+  try {
+    const data = await readFile(file);
+    const hpStringify = change_alias(data.hp);
+
+    await axios.post("/score/add-hp", {
+      hpStringify,
+      dataFilter: data.dataFilter,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const onChangeFile = async (e) => {
+  file = e.target.files[0];
+};
 
 const readFile = async (file) => {
-	return new Promise((resolve, reject) => {
-		try {
-			const reader = new FileReader();
-			reader.readAsBinaryString(file);
+  return new Promise((resolve, reject) => {
+    try {
+      const reader = new FileReader();
+      reader.readAsBinaryString(file);
 
-			reader.onload = (evt) => { // evt = on_file_select event
-				/* Parse data */
-				const bstr = evt.target.result;
-				const wb = XLSX.read(bstr, { type: 'binary' });
-				/* Get first worksheet */
-				const wsname = wb.SheetNames[0];
-				const ws = wb.Sheets[wsname];
-				/* Convert array of arrays */
-				const data = XLSX.utils.sheet_to_json(ws, { header: 1 });
-				const hp = data[0][1];
+      reader.onload = (evt) => {
+        // evt = on_file_select event
+        /* Parse data */
+        const bstr = evt.target.result;
+        const wb = XLSX.read(bstr, { type: "binary" });
+        /* Get first worksheet */
+        const wsname = wb.SheetNames[0];
+        const ws = wb.Sheets[wsname];
+        /* Convert array of arrays */
+        const data = XLSX.utils.sheet_to_json(ws, { header: 1 });
+        console.log(data);
+        const hp = data[0][1];
 
-				let dataFilter = []
-				if (data) {
-					for (let i = 2; i < data.length; i++) {
-						dataFilter.push({
-							id: data[i][1],
-							lastName: data[i][2],
-							firstName: data[i][3],
-							dayOfBirth: data[i][4],
-							mssv: data[i][5],
-							score: {
-								th: data[i][27] || 0,
-								lt: data[i][28] || 0,
-								qt: data[i][29] || 0,
-							}
-						})
-					}
-				}
-				resolve({ dataFilter, hp })
-			};
-		} catch (err) {
-			reject(err)
-		}
-	})
-}
+        let dataFilter = [];
+        if (data) {
+          for (let i = 2; i < data.length; i++) {
+            dataFilter.push({
+              id: data[i][1],
+              lastName: data[i][2],
+              firstName: data[i][3],
+              dayOfBirth:
+                typeof data[i][4] === "number"
+                  ? changeOrderDate(xlSerialToJsDate(data[i][4]))
+                  : changeOrderDate(data[i][4]),
+              mssv: data[i][5],
+              score: {
+                thucHanh: data[i][27] || 0,
+                lyThuyet: data[i][28] || 0,
+                quaTrinh: data[i][29] || 0,
+              },
+            });
+          }
+        }
+        resolve({ dataFilter, hp });
+      };
+    } catch (err) {
+      reject(err);
+    }
+  });
+};
 
-const onSubmitFormGetData = async e => {
-	e.preventDefault();
+const onSubmitFormGetData = async (e) => {
+  e.preventDefault();
 
-	let hp = document.getElementById("hp").value;
-	let mssv = document.getElementById("mssv").value;
+  let hp = document.getElementById("hp").value;
+  let mssv = document.getElementById("mssv").value;
+  let dayOfBirth = new Date(
+    document.getElementById("dayOfBirth").valueAsNumber
+  ).toLocaleDateString();
 
-	const res = await axios.post("/score/hp", { hp, mssv });
-	console.log(res)
+  const res = await axios.post("/score/hp", { hp, mssv, dayOfBirth });
+  console.log(res);
 
-	if (res.data) {
-		let rs = "Diem cua ban la \n";
-		for (let i in res.data.score) {
-			rs += i + ": " + res.data.score[i] + " || ";
-		}
-		alert(rs)
-	} else {
-		alert("Khong tim thay ket qua")
-	}
-}
+  if (res.data) {
+    let rs = "Diem cua ban la <br />";
+    for (let i in res.data.score) {
+      rs += i + ": " + res.data.score[i] + "<br />";
+    }
+    document.getElementById("results").innerHTML = rs;
+  } else {
+    alert("Khong tim thay ket qua");
+  }
+};
 
 window.onload = async () => {
-
-	document.getElementById("formAddData").addEventListener("submit", onSubmitFormAddData);
-	document.getElementById("formGetData").addEventListener("submit", onSubmitFormGetData);
-	document.getElementById("file").addEventListener("change", onChangeFile);
-}
+  document
+    .getElementById("formAddData")
+    .addEventListener("submit", onSubmitFormAddData);
+  document
+    .getElementById("formGetData")
+    .addEventListener("submit", onSubmitFormGetData);
+  document.getElementById("file").addEventListener("change", onChangeFile);
+};
